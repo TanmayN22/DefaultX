f41b73c7b03dc75341f4fcab3af798a84086742b
Tanmay Nayak <125031648+TanmayN22@users.noreply.github.com>
2025-07-28 00:48:17 +0530

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:defaultx/widgets/login_ui.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,12 +7,36 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'DefalutX',
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: Border(bottom: BorderSide(color: Colors.grey, width: 1)),
+        title: Align(
+          alignment: Alignment.topLeft,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 30),
+            child: Text(
+              "DefaultX",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
+      body: LoginUi(),
     );
   }
 }
